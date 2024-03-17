9fea935b33e8838820e05f9f1b42d7b5fbdb7033
sumionochi <aaditya.srivastava.connect@gmail.com>
2024-03-17 15:08:54 +0530

    cleaned some changes

---
+++ src/app/generateQues/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const GenerateQues = (props: Props) => {
+  return (
+    <div className="flex flex-col max-w-6xl mx-auto mt-10 gap-8 p-4 mb-4">
+        
+    </div>
+  )
+}
+
+export default GenerateQues
